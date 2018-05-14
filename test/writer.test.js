a022ec92105113b6a73dbc1622d26dd522fa12d6
Simen Bekkhus <sbekkhus91@gmail.com>
2018-05-14 13:50:57 +0200

    fix(package): use scoped asset-pipe-common module

--- test/writer.test.js
+++ test/writer.test.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const { hasher } = require('asset-pipe-common');
+const { hasher } = require('@asset-pipe/common');
 const { identifyCssModule, bundleCssModule } = require('../lib/util.js');
 const Writer = require('..');
 
