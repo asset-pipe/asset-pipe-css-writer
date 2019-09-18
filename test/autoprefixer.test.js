37a9d136c6d8bab7eaf11544e1a75f65062e08cc
Cody Olsen <cody.olsen@finn.no>
2019-09-18 13:54:37 +0200

    add autoprefixer snapshot tests

---
+++ test/autoprefixer.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const prettier = require('prettier');
+const { bundleCssModule } = require('../lib/util.js');
+
+describe('should use autoprefixer to remove old syntax', () => {
+    test('nybygg.css', async () => {
+        expect.assertions(3);
+        const filePath = path.join(__dirname, 'test-assets/nybygg.css');
+
+        let result = await bundleCssModule(filePath);
+        // cssnano compacts all the css to a single line, prettier expands it again so the diffs are easier to read
+        result = prettier.format(result, {
+            parser: 'css',
+        });
+
+        // expect(result).toMatch('my-module-3/main.css');
+        // expect(result).toMatch('my-module-3/dep.css');
+        expect(result).toMatchSnapshot();
+    });
+    test('frontpage-podium.css', async () => {
+        expect.assertions(3);
+        const filePath = path.join(
+            __dirname,
+            'test-assets/frontpage-podium.css'
+        );
+
+        let result = await bundleCssModule(filePath);
+        // cssnano compacts all the css to a single line, prettier expands it again so the diffs are easier to read
+        result = prettier.format(result, {
+            parser: 'css',
+        });
+
+        // expect(result).toMatch('my-module-3/main.css');
+        // expect(result).toMatch('my-module-3/dep.css');
+        expect(result).toMatchSnapshot();
+    });
+});
